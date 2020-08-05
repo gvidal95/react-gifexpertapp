7b67ed81d149bd2636f61cc46d20e2c0fc17f46e
gvidal <gabrielwv1095@gmail.com>
2020-08-04 23:52:28 -0500

    docs agregado

---
+++ docs/precache-manifest.2d76b959b83a4bda02c82785d186b12f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f6e4c8b6509256d48d348a1f9a9f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a5cc20da12fb310783",
+    "url": "/static/css/main.ad537529.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a5cc20da12fb310783",
+    "url": "/static/js/main.6e46a65c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
